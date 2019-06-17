0987292c5f1a55b2b9dd3e4d9eb27c5855ca8402
koktszfung <koktszfung@gmail.com>
2019-06-17 18:54:44 +0800

    allowed random init coord

--- get_data/pmg_data_gen.py
+++ get_data/pmg_data_gen.py
@@ -2,21 +2,21 @@ import numpy
 import pymatgen.core.lattice
 import pymatgen.core.structure
 from typing import List
+from typing import Union
 
 
 def frac_coords_from_sgnum(sgnum: int,
+                           init_coords: Union[List[List[float]], numpy.ndarray] = None,
                            scaling_factor=1,
-                           lattice_vectors: List[float] = None,
-                           species: List[str] = None,
-                           init_coords: List[List[float]] = None,
-                           ) -> numpy.ndarray:
-
+                           lattice_vectors: Union[List[float], List[List[float]]] = None,
+                           species: List[str] = None) -> numpy.ndarray:
+    if init_coords is None:
+        init_coords = [[0, 0, 0]]
     if lattice_vectors is None:
         lattice_vectors = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
     if species is None:
-        species = ["Si"]
-    if init_coords is None:
-        init_coords = [[0, 0, 0]]
+        species = ["Si"] * len(init_coords)
+
     lattice = pymatgen.core.lattice.Lattice(
         [x * scaling_factor for x in lattice_vectors]
     )
