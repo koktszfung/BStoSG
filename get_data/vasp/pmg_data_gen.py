130922cf064380fec1b615961dddea63f5bad3f4
koktszfung <koktszfung@gmail.com>
2019-06-18 19:29:14 +0800

    fixed bugs about scaling factor and added elem to dump_poscar

--- get_data/vasp/pmg_data_gen.py
+++ get_data/vasp/pmg_data_gen.py
@@ -5,11 +5,11 @@ from typing import List
 from typing import Union
 
 
-def frac_coords_from_sgnum(sgnum: int,
-                           init_coords: Union[List[List[float]], numpy.ndarray] = None,
-                           scaling_factor=1,
-                           lattice_vectors: Union[List[float], List[List[float]]] = None,
-                           species: List[str] = None) -> numpy.ndarray:
+def struct_from_sgnum(sgnum: int,
+                      init_coords: Union[List[List[float]], numpy.ndarray] = None,
+                      scaling_factor: float = 1,
+                      lattice_vectors: Union[List[float], List[List[float]]] = None,
+                      species: List[str] = None) -> pymatgen.core.structure.IStructure:
     if init_coords is None:
         init_coords = [[0, 0, 0]]
     if lattice_vectors is None:
@@ -18,13 +18,13 @@ def frac_coords_from_sgnum(sgnum: int,
         species = ["Si"] * len(init_coords)
 
     lattice = pymatgen.core.lattice.Lattice(
-        [x * scaling_factor for x in lattice_vectors]
+        [x * scaling_factor for vector in lattice_vectors for x in vector]
     )
     structure = pymatgen.core.structure.IStructure.from_spacegroup(
         sgnum, lattice, species, init_coords
     )
-    return structure.frac_coords
+    return structure
 
 
 if __name__ == "__main__":
-    print(frac_coords_from_sgnum(1))
+    print(struct_from_sgnum(1).frac_coords)
