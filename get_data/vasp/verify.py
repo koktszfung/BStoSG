2d18768a7e0e96ec9a448c1793988de2800026b6
koktszfung <koktszfung@gmail.com>
2019-06-22 12:36:51 +0800

    fix gitignore

--- get_data/vasp/verify.py
+++
@@ -1,8 +0,0 @@
-from pymatgen.core.lattice import Lattice
-from pymatgen.core.structure import IStructure
-from gen_vasp_input import struct_from_sgnum, write_vasp_input
-
-
-struct = struct_from_sgnum(227, 3.867, init_coords=[[0.2, 0.2, 0.2]])
-print("\n", struct.lattice.angles)
-write_vasp_input(struct, )
