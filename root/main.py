ece69897bd6bc8bd93e4826a0bb096b13e369bf9
koktszfung <koktszfung@gmail.com>
2019-07-08 21:45:12 +0800

    added result for EL and D

--- root/main.py
+++ root/main.py
@@ -75,14 +75,8 @@ def main_crys2sg_one(crysnum: int, num_epoch: int = 1):
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.75)
     criterion = torch.nn.CrossEntropyLoss()
 
-    # dataset = data_loader.SetCrys2Sg(["data/actual/spacegroup_list_{}.txt".format(i) for i in
-    #                                   crystal.spacegroup_number_range(crysnum)], crysnum, 0.1)
-    #
     dataset = data_loader.SetCrys2Sg(["data/actual/spacegroup_list_{}.txt".format(i) for i in
-                                      range(1, 231)], crysnum, 0.1)
-
-    # dataset = data_loader.SetCrys2Sg(["data/guess/crystal_list_{}.txt".format(crysnum)
-    #                                   ], crysnum, 0.1)
+                                      crystal.spacegroup_number_range(crysnum)], crysnum, 0.1)
 
     train_loader, valid_loader = data_loader.get_valid_train_loader(dataset, 32)
 
@@ -103,37 +97,38 @@ def main_crys2sg_all(num_epoch: int = 1):
 
 
 if __name__ == "__main__":
-    my_seed = 1155110044
-    numpy.random.seed(my_seed)
-    torch.manual_seed(my_seed)
+    # my_seed = 1155110044
+    # numpy.random.seed(my_seed)
+    # torch.manual_seed(my_seed)
 
-    # prepare actual data #
-    # data_processing.create_valid_list_files(30, "data/new_input_data_2/", "data/actual/valid_list.txt")
+    # prepare actual data # (Do this everytime dataset is changed)
+    # data_processing.create_valid_list_files(30, "data/hw_input_data_3/", "data/actual/valid_list.txt")
     # data_processing.create_actual_crystal_list_files("data/actual/valid_list.txt", "data/actual/")
     # data_processing.create_actual_spacegroup_list_files("data/actual/valid_list.txt", "data/actual/")
 
     # generate guess data #
     # main_bs2sg(num_epoch=10)
     # main_bs2crys(num_epoch=10)
-    main_crys2sg_all(num_epoch=10)
+    # main_crys2sg_all(num_epoch=10)
 
     # analyse result #
-    # analysis.print_result(range(1, 8), "data/guess/", "data/actual/", "crystal_list_{}.txt")
-    analysis.print_result(range(1, 231), "data/guess/", "data/actual/", "spacegroup_list_{}.txt")
-    # for c in range(1, 8):
-    #     print("crystal system {}".format(c))
-    #     print("\nbandstructure to crystal system result")
-    #     analysis.print_result(
-    #         [c], "data/guess/", "data/actual/", "crystal_list_{}.txt"
-    #     )
-    #     print("\ncrystal to spacegroup system result")
-    #     analysis.print_result(
-    #         crystal.spacegroup_number_range(c), "data/guess/", "data/actual/", "spacegroup_list_{}.txt"
-    #     )
-    #     print("\n")
+    # analysis.print_result(range(1, 8), "data/guess/", "data/actual/", "crystal_list_{}.txt")  # into crys
+    analysis.print_result(range(1, 231), "data/guess/", "data/actual/", "spacegroup_list_{}.txt")  # into sg
+    for c in range(1, 8):  # into crys and sg
+        print("crystal system {}".format(c))
+        print("\nbandstructure to crystal system result")
+        analysis.print_result(
+            [c], "data/guess/", "data/actual/", "crystal_list_{}.txt"
+        )
+        print("\ncrystal to spacegroup system result")
+        analysis.print_result(
+            crystal.spacegroup_number_range(c), "data/guess/", "data/actual/", "spacegroup_list_{}.txt"
+        )
+        print("\n")
 
     torch.cuda.empty_cache()
     import winsound
+
     duration = 500  # milliseconds
     freq = 200  # Hz
     winsound.Beep(freq, duration)
